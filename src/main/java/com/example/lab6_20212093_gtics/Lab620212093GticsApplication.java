6d6cc619832edf9bda03b342dc2279631f10cf55
TroTurst <a20212093@pucp.edu.pe>
2025-10-02 21:10:52 -0500

    Primer commit

---
+++ src/main/java/com/example/lab6_20212093_gtics/Lab620212093GticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab6_20212093_gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab620212093GticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab620212093GticsApplication.class, args);
+    }
+
+}
